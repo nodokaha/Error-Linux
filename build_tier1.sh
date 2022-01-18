85425f6f1bfaa8a2d460e058c801929ac8859b1e
SCP-EEE <scpeee@wail.ch>
2022-01-18 16:21:52 +0900

    go!

--- build_tier1.sh
+++ build_tier1.sh
@@ -1,1 +1,21 @@
 source $(pwd)/.bashrc
+cd $LFS/build
+tar xvf $LFS/sources/glibc*.xz
+cd glibc-2.34
+case $(uname -m) in
+i?86)
+ln -sfv ld-linux.so.2 $LFS/lib/ld-lsb.so.3
+;;
+x86_64) ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64
+ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64/ld-lsb-x86-64.so.3
+;;
+esac
+patch -Np1 -i $LFS/sources/glibc*.patch
+mkdir -v build
+cd build
+echo "rootsbindir=/usr/sbin" > configparms
+../configure --prefix=/usr --host=$LFS_TGT --build=$(../scripts/config.guess) --enable-kernel=3.2 --with-headers=$LFS/usr/include libc_cv_slibdir=/usr/lib
+make
+make DESTDIR=$LFS install
+sed '/RTLDLIST=/s@/usr@@g' -i $LFS/usr/bin/ldd
+$LFS/tools/libexec/gcc/$LFS_TGT/11.2.0/install-tools/mkheaders
---
+++ build_tier2.sh
@@ -0,0 +1,50 @@
+source $(pwd)/.bashrc
+cd $LFS/build
+cd gcc-11.2.0
+cd build
+../libstdc++-v3/configure --host=$LFS_TGT --build=$(../config.guess) --prefix=/usr --disable-multilib --disable-nls --disable-libstdcxx-pch --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/11.2.0
+make
+make DESTDIR=$LFS install
+cd $LFS/build
+tar xvf $LFS/sources/m4*
+cd m4-1.4.19
+./configure --prefix=/usr --host=$LFS_TGT --build=$(build-aux/config.guess)
+make
+make DESTDIR=$LFS install
+cd $LFS/build
+tar xvf $LFS/sources/ncurses*
+cd ncurses-6.2
+sed -i s/mawk// configure
+mkdir build
+pushd build
+../configure
+make -C include
+make -C progs tic
+popd
+./configure --prefix=/usr --host=$LFS_TGT --build=$(./config.guess) --mandir=/usr/share/man --with-manpage-format=normal --with-shared --without-debug --without-ada --without-normal --enable-widec
+make
+make DESTDIR=$LFS TIC_PATH=$(pwd)/build/progs/tic install
+echo "INPUT(-lncursesw)" > $LFS/usr/lib/libncurses.so
+cd $LFS/build
+tar xvf $LFS/sources/bash*
+cd bash-5.1.8
+./configure --prefix=/usr --build=$(support/config.guess) --host=$LFS_TGT --without-bash-malloc
+make
+make DESTDIR=$LFS install
+ln -sv bash $LFS/bin/sh
+cd $LFS/build
+tar xvf $LFS/sources/coreutils*.xz
+cd coreutils-8.32
+./configure --prefix=/usr --host=$LFS_TGT --build=$(build-aux/config.guess) --enable-install-program=hostname --enable-no-install-program=kill,uptime
+make
+make DESTDIR=$LFS install
+mv -v $LFS/usr/bin/chroot $LFS/usr/sbin
+mkdir -pv $LFS/usr/share/man/man8 
+mv -v $LFS/usr/share/man/man1/chroot.1 $LFS/usr/share/man/man8/chroot.8
+sed -i 's/"1"/"8"/' $LFS/usr/share/man/man8/chroot.8
+cd $LFS/build
+tar xvf $LFS/sources/diffutils*
+cd diffutils-3.8
+./configure --prefix=/usr --host=$LFS_TGT
+make
+make DESTDIR=$LFS install
