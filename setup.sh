fcb0f339c9a2a3a5573642409a37fdd18f265a58
SCP-EEE <scpeee@wail.ch>
2022-02-18 19:48:21 +0900

    mini fix

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-mkdir -p lfs/sources
 export LFS=$(pwd)/lfs
 if [ ! -d $LFS/sources ]; then 
+mkdir -p lfs/sources
 chmod -v a+wt $LFS/sources
 wget --input-file=wget-list --continue --directory-prefix=$LFS/sources
-; fi
+fi
 pushd $LFS/sources
 md5sum -c ../../md5sums
 popd
