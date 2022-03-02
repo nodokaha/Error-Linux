615ae0e6d608435e03eb5ff2402a008e6793d3e2
SCP-EEE <scpeee@wail.ch>
2022-03-02 18:43:43 +0900

    update

--- error_linux.sh
+++ error_linux.sh
@@ -81,10 +81,10 @@ tar -xf ../../tzdata2021a.tar.gz
 ZONEINFO=/usr/share/zoneinfo
 mkdir -pv $ZONEINFO/{posix,right}
 for tz in etcetera southamerica northamerica europe africa antarctica
-asia australasia backward; do
-zic -L /dev/null -d $ZONEINFO ${tz}
-zic -L /dev/null -d $ZONEINFO/posix ${tz}
-zic -L leapseconds -d $ZONEINFO/right ${tz}
+	  asia australasia backward; do
+    zic -L /dev/null -d $ZONEINFO ${tz}
+    zic -L /dev/null -d $ZONEINFO/posix ${tz}
+    zic -L leapseconds -d $ZONEINFO/right ${tz}
 done
 cp -v zone.tab zone1970.tab iso3166.tab $ZONEINFO
 zic -d $ZONEINFO -p America/New_York
@@ -215,3 +215,198 @@ install -v -dm755 /usr/share/doc/dejagnu-1.6.3
 install -v -m644 doc/dejagnu.{html,txt} /usr/share/doc/dejagnu-1.6.3
 make check
 cd /build
+tar xvf /sources/binutils-2.37.tar.xz
+cd binutils-2.37
+patch -Np1 -i /sources/binutils-2.37-upstream_fix-1.patch
+sed -i '63d' etc/texi2pod.pl
+find -name \*.1 -delete
+mkdir -v build
+cd build
+../configure --prefix=/usr --enable-gold --enable-ld=default --enable-plugins --enable-shared --disable-werror --enable-64-bit-bfd --with-system-zlib
+make tooldir=/usr
+make -k check
+make tooldir=/usr install -j1
+rm -fv /usr/lib/lib{bfd,ctf,ctf-nobfd,opcodes}.a
+cd /build
+tar xvf /sources/gmp*
+cd gmp-6.2.1
+./configure --prefix=/usr --enable-cxx --disable-static --docdir=/usr/share/doc/gmp-6.2.1
+make
+make html
+make check 2>&1 | tee gmp-check-log
+awk '/# PASS:/{total+=$3} ; END{print total}' gmp-check-log
+make install
+make install-html
+cd /build
+tar xvf /sources/mpfr*
+cd mpfr-4.1.0
+./configure --prefix=/usr --disable-static --enable-thread-safe --docdir=/usr/share/doc/mpfr-4.1.0
+make
+make html
+make check
+make install
+make install-html
+cd /build
+tar xvf /sources/mpc*
+cd mpc-1.2.1
+./configure --prefix=/usr --disable-static --docdir=/usr/share/doc/mpc-1.2.1
+make
+make html
+make check
+make install
+make install-html
+cd /build
+tar xvf /sources/attr*
+cd attr-2.5.1
+./configure --prefix=/usr --disable-static --sysconfdir=/etc --docdir=/usr/share/doc/attr-2.5.1
+make
+make check
+make install
+cd /build
+tar xvf /sources/acl*
+cd acl-2.3.1
+./configure --prefix=/usr --disable-static --docdir=/usr/share/doc/acl-2.3.1
+make
+make install
+cd /build
+tar xvf /sources/libcap*
+cd libcap-2.53
+sed -i '/install -m.*STA/d' libcap/Makefile
+make prefix=/usr lib=lib
+make test
+make prefix=/usr lib=lib install
+chmod -v 755 /usr/lib/lib{cap,psx}.so.2.53
+cd /build
+tar xvf /sources/shadow*
+cd shadow-4.9
+sed -i 's/groups$(EXEEXT) //' src/Makefile.in
+find man -name Makefile.in -exec sed -i 's/groups\.1 / /' {} \;
+find man -name Makefile.in -exec sed -i 's/getspnam\.3 / /' {} \;
+find man -name Makefile.in -exec sed -i 's/passwd\.5 / /' {} \;
+sed -e 's:#ENCRYPT_METHOD DES:ENCRYPT_METHOD SHA512:' -e 's:/var/spool/mail:/var/mail:' -e '/PATH=/{s@/sbin:@@;s@/bin:@@}' -i etc/login.defs
+sed -e "224s/rounds/min_rounds/" -i libmisc/salt.c
+touch /usr/bin/passwd
+./configure --sysconfdir=/etc --with-group-name-max-length=32
+make
+make exec_prefix=/usr install
+make -C man install-man
+mkdir -p /etc/default
+useradd -D --gid 999
+pwconv
+grpconv
+sed -i 's/yes/no/' /etc/default/useradd
+cd /build
+tar xvf /sources/gcc*
+cd gcc-11.2.0
+sed -e '/static.*SIGSTKSZ/d' -e 's/return kAltStackSize/return SIGSTKSZ * 4/' -i libsanitizer/sanitizer_common/sanitizer_posix_libcdep.cpp
+case $(uname -m) in
+    x86_64)
+	sed -e '/m64=/s/lib64/lib/' -i.orig gcc/config/i386/t-linux64
+	;;
+esac
+mkdir -v build
+cd build
+../configure --prefix=/usr LD=ld --enable-languages=c,c++ --disable-multilib --disable-bootstrap --with-system-zlib
+make
+ulimit -s 32768
+chown -Rv tester .
+su tester -c "PATH=$PATH make -k check"
+../contrib/test_summary
+make install
+rm -rf /usr/lib/gcc/$(gcc -dumpmachine)/11.2.0/include-fixed/bits/
+chown -v -R root:root /usr/lib/gcc/*linux-gnu/11.2.0/include{,-fixed}
+ln -svr /usr/bin/cpp /usr/lib
+ln -sfv ../../libexec/gcc/$(gcc -dumpmachine)/11.2.0/liblto_plugin.so /usr/lib/bfd-plugins/
+echo 'int main(){}' > dummy.c
+cc dummy.c -v -Wl,--verbose &> dummy.log
+readelf -l a.out | grep ': /lib'
+grep -o '/usr/lib.*/crt[1in].*succeeded' dummy.log
+grep -B4 '^ /usr/include' dummy.log
+grep 'SEARCH.*/usr/lib' dummy.log |sed 's|; |\n|g'
+grep "/lib.*/libc.so.6 " dummy.log
+grep found dummy.log
+rm -v dummy.c a.out dummy.log
+mkdir -pv /usr/share/gdb/auto-load/usr/lib
+mv -v /usr/lib/*gdb.py /usr/share/gdb/auto-load/usr/lib
+cd /build
+tar xvf /sources/pkg-config*
+cd pkg-config-0.29.2
+./configure --prefix=/usr --with-internal-glib --disable-host-tool --docdir=/usr/share/doc/pkg-config-0.29.2
+make
+make check
+make install
+cd /build
+tar xvf /sources/ncurses*
+cd ncurses-6.2
+./configure --prefix=/usr --mandir=/usr/share/man --with-shared --without-debug --without-normal --enable-pc-files --enable-widec
+make
+make install
+for lib in ncurses form panel menu ; do
+    rm -vf /usr/lib/lib${lib}.so
+    echo "INPUT(-l${lib}w)" > /usr/lib/lib${lib}.so
+    ln -sfv ${lib}w.pc /usr/lib/pkgconfig/${lib}.pc
+done
+rm -vf /usr/lib/libcursesw.so
+echo "INPUT(-lncursesw)" > /usr/lib/libcursesw.so
+ln -sfv libncurses.so /usr/lib/libcurses.so
+rm -fv /usr/lib/libncurses++w.a
+mkdir -v /usr/share/doc/ncurses-6.2
+cp -v -R doc/* /usr/share/doc/ncurses-6.2
+make distclean
+./configure --prefix=/usr --with-shared --without-normal --without-debug --without-cxx-binding --with-abi-version=5
+make sources libs
+cp -av lib/lib*.so.5* /usr/lib
+cd /build
+tar xvf /sources/sed*
+cd sed-4.8
+./configure --prefix=/usr
+make
+make html
+chown -Rv tester .
+su tester -c "PATH=$PATH make check"
+make install
+install -d -m755 /usr/share/doc/sed-4.8
+install -m644 doc/sed.html /usr/share/doc/sed-4.8
+cd /build
+tar xvf /sources/psmisc*
+cd psmisc-23.4
+./configure --prefix=/usr
+make
+make install
+cd /build
+tar xvf /sources/gettext*
+cd gettext-0.21
+./configure --prefix=/usr --disable-static --docdir=/usr/share/doc/gettext-0.21
+make
+make check
+make install
+chmod -v 0755 /usr/lib/preloadable_libintl.so
+cd /build
+tar xvf /sources/bison*
+cd bison-3.7.6
+./configure --prefix=/usr --docdir=/usr/share/doc/bison-3.7.6
+make
+make check
+make install
+cd /build
+tar xvf /sources/grep*
+cd grep-3.7
+./configure --prefix=/usr
+make
+make check
+make install
+cd /build
+tar xvf /sources/bash*
+cd bash-5.1.8
+./configure --prefix=/usr --docdir=/usr/share/doc/bash-5.1.8 --without-bash-malloc --with-installed-readline
+make
+chown -Rv tester .
+su -s /usr/bin/expect tester << EOF
+set timeout -1
+spawn make tests
+expect eof
+lassign [wait] _ _ _ value
+exit $value
+EOF
+make install
+exec /bin/bash --login +h
